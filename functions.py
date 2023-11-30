bda1c4f4f4fe0f1da548fffc91d2b1b0148f0012
Oliver-Insignia <152512010+Oliver-Insignia@users.noreply.github.com>
2023-11-30 14:49:43 +0000

    Initial Commit

---
+++ functions.py
@@ -0,0 +1,33 @@
+import json
+import os
+
+
+def create_assistant(client):
+  assistant_file_path = 'assistant.json'
+
+  if os.path.exists(assistant_file_path):
+    with open(assistant_file_path, 'r') as file:
+      assistant_data = json.load(file)
+      assistant_id = assistant_data['assistant_id']
+      print("Loaded existing assistant ID.")
+  else:
+    file = client.files.create(file=open("knowledge.docx", "rb"),
+                               purpose='assistants')
+
+    assistant = client.beta.assistants.create(instructions="""
+          The assistant, Pet Remedy Assistant, has been programmed to help customers with common questions about how the products work, science around the products delivery questions and any other questions inside the document.
+A document has been provided with information on Pet Remedy info. Please don't mention the document though.
+          """,
+                                              model="gpt-3.5-turbo-1106",
+                                              tools=[{
+                                                  "type": "retrieval"
+                                              }],
+                                              file_ids=[file.id])
+
+    with open(assistant_file_path, 'w') as file:
+      json.dump({'assistant_id': assistant.id}, file)
+      print("Created a new assistant and saved the ID.")
+
+    assistant_id = assistant.id
+
+  return assistant_id
